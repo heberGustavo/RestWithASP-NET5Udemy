f65dc605653b17cd899bd87b70156d36d477b031
heberGustavo <heber.gbarbosa@hotmail.com>
2022-12-19 17:37:16 -0300

    Create Interface IPersonService

---
+++ RestWithASPNETUdemy_ScaffoldCalculadora/RestWithASP_NET5Udemy/RestWithASP_NET5Udemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASP_NET5Udemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5Udemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
