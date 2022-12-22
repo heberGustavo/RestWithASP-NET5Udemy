c52f626db73261d4a103a115fb4810c9cb12c1a0
heberGustavo <heber.gbarbosa@hotmail.com>
2022-12-22 12:13:17 -0300

    Create VO and Converter to Person

---
+++ RestWithASPNETUdemy_ScaffoldCalculadora/RestWithASP_NET5Udemy/RestWithASP_NET5Udemy/Data/Converter/Implementations/PersonConverter.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASP_NET5Udemy.Data.Converter.Contract;
+using RestWithASP_NET5Udemy.Data.VO;
+using RestWithASP_NET5Udemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5Udemy.Data.Converter.Implementations
+{
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origem)
+        {
+            if (origem == null) return null;
+
+            return new Person
+            {
+                Id = origem.Id,
+                FirstName = origem.FirstName,
+                LastName = origem.LastName,
+                Address = origem.Address,
+                Gender = origem.Gender
+            };
+
+        }
+
+        public PersonVO Parse(Person origem)
+        {
+            if (origem == null) return null;
+
+            return new PersonVO
+            {
+                Id = origem.Id,
+                FirstName = origem.FirstName,
+                LastName = origem.LastName,
+                Address = origem.Address,
+                Gender = origem.Gender
+            };
+        }
+
+        public List<PersonVO> Parse(List<Person> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<Person> Parse(List<PersonVO> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
