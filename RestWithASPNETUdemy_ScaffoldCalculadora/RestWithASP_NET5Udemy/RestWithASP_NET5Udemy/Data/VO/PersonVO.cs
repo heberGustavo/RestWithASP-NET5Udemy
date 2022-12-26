0f33e1217aa3d56850ef04f4030c3e9fa27180a5
heberGustavo <heber.gbarbosa@hotmail.com>
2022-12-26 11:54:04 -0300

    Custom Serialization

--- RestWithASPNETUdemy_ScaffoldCalculadora/RestWithASP_NET5Udemy/RestWithASP_NET5Udemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy_ScaffoldCalculadora/RestWithASP_NET5Udemy/RestWithASP_NET5Udemy/Data/VO/PersonVO.cs
@@ -1,15 +1,22 @@
-﻿namespace RestWithASP_NET5Udemy.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASP_NET5Udemy.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public int Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
-        
+
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
-        
+
+        [JsonIgnore]
         public string Address { get; set; }
-        
+
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
